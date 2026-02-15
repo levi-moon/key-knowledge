6e040017c7a2cd1c65eee988f669f363a7fe6d7f
levi <jinzhao1991@yeah.net>
2026-02-15 16:32:44 +0800

    添加责任链模式

---
+++ src/main/java/com/levi/knowledge/design_patterns/behavioral_pattern/chain_of_responsibility_pattern/ConsoleLogger.java
@@ -0,0 +1,30 @@
+package com.levi.knowledge.design_patterns.behavioral_pattern.chain_of_responsibility_pattern;
+
+/**
+ * @Class: ConsoleLogger
+ * @Description: 控制台日志
+ * @Author: Levi
+ * @Date: 2026/2/15 16:25
+ * @Version: 1.0
+ */
+public class ConsoleLogger extends AbstractLogger {
+
+    /**
+     * 有参构造器
+     *
+     * @param level 日志等级
+     */
+    public ConsoleLogger(int level) {
+        this.level = level;
+    }
+
+    /**
+     * 记录
+     *
+     * @param message
+     */
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
