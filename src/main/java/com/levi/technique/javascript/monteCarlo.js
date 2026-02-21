5065e474fdbee291e5cd28fd3844caee3f2fbf85
levi <jinzhao1991@yeah.net>
2026-02-21 21:06:06 +0800

    蒙特卡洛模拟

--- src/main/java/com/levi/technique/javascript/monteCarlo.js
+++ src/main/java/com/levi/technique/javascript/monteCarlo.js
@@ -1,9 +1,9 @@
 /**
  * 边长为2的正方形，内有一个圆。
- * 则圆的半径为1，则圆的面积为Π；
+ * 则圆的半径为1，则圆的面积为π；
  * 正方形的面积为2*2=4。
- * 圆面积/正方形面积=Π/4。
- * 在正方形内点n个点，则落在圆内的点的数量与总的点数的比值为Π/4
+ * 圆面积/正方形面积=π/4。
+ * 在正方形内点n个点，则落在圆内的点的数量与总的点数的比值为π/4
  * @param numSamples
  * @returns {number}
  */
@@ -22,4 +22,8 @@ function estimatePi(numSamples) {
     return 4 * ratio; // π = 4 * (Area of circle / Area of square)
 }
 
-console.log("Estimated π:", estimatePi(100000));
+window.onload = function () {
+    var val = estimatePi(100000);
+    console.log("Estimated π:", val);
+    document.getElementById('monteCarloValus').innerHTML = val;
+}
